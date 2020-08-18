be93c0c416338d8c438105c0322c0bd82929f2b9
Azalea-oz <ssfiress2092@gmail.com>
2020-08-19 00:14:43 +0900

    不必要なものを削除

--- GUI/DEBUG/AzDebug.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include"debug.hpp"
-//#include"debug.cpp"
--- GUI/DEBUG/time.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include<iostream>
-#include<ctime>
-#include<chrono>
-
-namespace Time{
-	void NowTime();
-}
